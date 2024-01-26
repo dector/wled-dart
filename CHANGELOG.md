b0cffa662b1b63ee7dd8c81c5d8f0a41a35b436b
Dan <code@dector.space>
2024-01-26 13:17:17 +0100

    feat: add on/off methods

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
-## 0.0.1
+## [0.0.2]
 
-New:
-    - WLED API Controller.
-    - Toggle method.
+### New
+- Turn on/off methods.
+
+## [0.0.1] - 25 Jan 2024
+
+### New
+- WLED API Controller.
+- Toggle method.
--- lib/wled.dart
+++ lib/wled.dart
@@ -1,13 +1,30 @@
 import 'dart:io' show HttpClient, HttpClientResponse;
 
+/// Use this class to send commands to WLED instance.
+///
+/// It implements simple stateless request-response mechanism
+/// and don't reuse connection between commands.
+/// Use it for one shot operations.
 class Wled {
+  /// Host
   final String host;
 
-  late final HttpClient _http;
+  late final HttpClient _http = HttpClient();
 
   /// Create new WLED control.
-  Wled(this.host) {
-    _http = HttpClient();
+  ///
+  /// [host] - is a hostname (e.g. `localhost`)
+  /// or an IPv4 address (e.g. `192.168.1.123`).
+  Wled(this.host);
+
+  /// Turn off LED.
+  Future<void> turnOff() async {
+    await _request([_Op.turnOff]);
+  }
+
+  /// Turn on LED.
+  Future<void> turnOn() async {
+    await _request([_Op.turnOn]);
   }
 
   /// Toggle LED.
@@ -26,17 +43,21 @@ class Wled {
 
 extension _OpExt on List<_Op> {
   String asParameters() {
-    return map((it) => switch (it) {
-          _Op.toggle => '&T=2',
-        }).join();
+    if (isEmpty) return '';
+
+    // ignore: prefer_interpolation_to_compose_strings
+    return '&' + map((it) => '${it.name}=${it.value}').join('&');
   }
 }
 
 enum _Op {
-  toggle('&T=2'),
+  turnOff('T', '0'),
+  turnOn('T', '1'),
+  toggle('T', '2'),
   ;
 
-  const _Op(this.arg);
+  const _Op(this.name, this.value);
 
-  final String arg;
+  final String name;
+  final String value;
 }
